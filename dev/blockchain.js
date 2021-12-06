2391d09c63c9b1fe9c702e6bb262a59bfc533bb4
Bora Lee <learnerbr@gmail.com>
2021-12-07 01:35:53 +0900

    [17] Hash Block Method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,3 +1,5 @@
+const sha256 = require('sha256');
+
 function Blockchain() {
   this.chain = [];
   this.pendingTransactions = [];  // pending transactions
@@ -36,6 +38,14 @@ Blockchain.prototype.createNewTransaction = function (amount, sender, recipient)
 
   return this.getLastBlock()['index'] + 1;
 }
+
+Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, nonce) {
+  const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData); 
+  const hash = sha256(dataAsString);
+  return hash;
+}
+
+
 
 
 module.exports = Blockchain;
