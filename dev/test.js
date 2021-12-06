df4688c81b08bc5d6b0df64ab937e8515ccf3eb4
Bora Lee <learnerbr@gmail.com>
2021-12-07 01:54:20 +0900

    [18] Testing Hash Block Method

--- dev/test.js
+++ dev/test.js
@@ -2,6 +2,7 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
+/* 
 bitcoin.createNewBlock(2389, 'OINA90SDNF90N', 'QWERTYUIOPASDFGHJKLZXCVBNM');
 
 bitcoin.createNewTransaction(100, 'ALEXLFKJHBDYF', 'JUANLSKDJFLKJHSDF');
@@ -16,3 +17,31 @@ bitcoin.createNewBlock(2899, 'BIJDHFLJNDLOFUH', 'UNVBLKJUBLFIUHL>S');
 console.log(bitcoin);
 console.log(bitcoin.chain[1].transactions); // 1 transaction
 console.log(bitcoin.chain[2].transactions); // 3 transactions
+
+**/
+
+const previousBlockHash = 'LKNDKFHLKJBKLKJHBFDLKJUHSDFFD';
+const currentBlockData = [
+  {
+    amout: 10,
+    sender: 'DSLKFBA<KJDBF',
+    recipient: 'VLKSUDBVKJBDV',
+  },
+  {
+    amout: 23,
+    sender: 'VSLKJBNVKJDF',
+    recipient: 'BOIUHSDN>FKJBFD',
+  },
+  {
+    amout: 4,
+    sender: 'VLIUHSDLKJFHSD',
+    recipient: 'BOIHJDS:LFKJ',
+  },
+];
+
+const nonce = 100;
+
+const hashedBlock = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+console.log(hashedBlock);
+
