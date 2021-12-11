d2454fef1f1da6c003056d0f07101f84285517f3
Bora Lee <learnerbr@gmail.com>
2021-12-12 00:33:40 +0900

    [29] Building the "GET /blockchain" Endpoint

--- dev/api.js
+++ dev/api.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
  
 app.get('/blockchain', function (req, res) {
-
+  res.send(bitcoin);
 })
 
 app.post('/transaction', function (req, res) {
