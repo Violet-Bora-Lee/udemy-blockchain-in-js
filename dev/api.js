edeaae415a1989ec0efab8febd48a0e20cb8ab76
Bora Lee <learnerbr@gmail.com>
2021-12-12 00:03:15 +0900

    [27] Building the API Foundation

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/blockchain', function (req, res) {
+
 })
+
+app.post('/transaction', function (req, res) {
+  
+})
+
+app.get('/mine', function (req, res) {
+  
+})
+
  
-app.listen(3000)
+app.listen(3000, function() {
+  console.log('Listining on port 3000...');
+})
