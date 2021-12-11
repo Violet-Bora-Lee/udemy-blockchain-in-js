2ec5121b80f55fb36b097cde7683dabe4b630817
Bora Lee <learnerbr@gmail.com>
2021-12-12 00:22:20 +0900

    [28] Installing Postman & Body-Parser

--- dev/api.js
+++ dev/api.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
  
 app.get('/blockchain', function (req, res) {
 
 })
 
 app.post('/transaction', function (req, res) {
-  
+  console.log(req.body);
+  res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`)
 })
 
 app.get('/mine', function (req, res) {
   
 })
-
  
 app.listen(3000, function() {
   console.log('Listining on port 3000...');
