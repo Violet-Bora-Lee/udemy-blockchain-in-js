ce386cbd8eb64378378245daafa1c64552ddfcbf
Bora Lee <learnerbr@gmail.com>
2021-12-21 01:59:08 +0900

    [38, 39] Building "POST /register-and-broadcast-node" Endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const {v1: uuid} = require('uuid');
 const port = process.argv[2];
+const rp = require('request-promise');
 
 const nodeAddress = uuid().split('-').join('');
 
@@ -44,6 +45,35 @@ app.get('/mine', function (req, res) {
 // register a node and "broadcast" it to the entire network
 app.post('/register-and-broadcast-node', function(req, res) {
   const newNodeUrl = req.body.newNodeUrl;
+  if (bitcoin.networkNodes.indexOf(newNodeUrl) === -1) bitcoin.networkNodes.push(newNodeUrl);
+  
+  const regNodesPromises = [];
+  
+  bitcoin.networkNodes.forEach(networkNodeUrl => {
+    // hitting '/register-node' endpoint
+    const requestOptions = {
+      uri: networkNodeUrl + '/register-node',
+      method: 'POST',
+      body: {newNodeUrl},
+      json: true,
+    };
+    regNodesPromises.push(rp(requestOptions));
+  });
+  
+  Promise.all(regNodesPromises)
+  .then(data => {
+    const bulkRegisterOptions = {
+      uri: newNodeUrl + '/register-nodes-bulk',
+      method: 'POST',
+      body: { allNetworkNodes: [...bitcoin.networkNodes, bitcoin.currentNodeUrl] },
+      json: true,
+    };
+    return rp(bulkRegisterOptions);
+  })
+  .then(data => {
+    res.json({note: 'New node registered with network successfully'})
+  })
+  
 });
 
 // 위 메서드와 다른점은, broadcast는 한번만 일어나면 되기 때문에, 브로드캐스트가 일어난 노드는 그냥 등록만 마치면 되므로 별도의 메서드가 필요하다.
